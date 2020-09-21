16f0b7b7c1d919088e14a91fd9a1f306275ed2f0
LuisDiego10 <66862458+LuisDiego10@users.noreply.github.com>
2020-09-21 17:10:44 -0600

    Mejora de interfaz Cliente y creación de variables para enviar información como el nombre del cliente recibe y el nombre de cliente envía

--- src/chat/Cliente.java
+++ src/chat/Cliente.java
@@ -1,9 +1,11 @@
 package chat;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.*;
+import  javax.swing.ImageIcon;
 
 public class Cliente {
 
@@ -15,17 +17,33 @@ public class Cliente {
 class ventanaCliente extends JFrame {
     public ventanaCliente(){
         this.setTitle("CLIENTE");
-        setBounds(600,300,280,350);
+        setBounds(600,300,620,650);
         canvasCliente canvasc=new canvasCliente();
         add(canvasc);
         setVisible(true);
         }
 }
 class canvasCliente extends JPanel{
+    private JTextField areaTexto,nombre,contacto;
+    private JTextArea areaMensajes_c;
+    private JButton enviarbtn;
     public canvasCliente(){
-        JLabel etiqueta=new JLabel("DIGITE EL MENSAJE QUE DESEA ENVIAR");
-        add(etiqueta);
-        areaTexto= new JTextField(25);
+        ImageIcon usericon =new ImageIcon("/home/diego/Algoritmos y Estructuras de Datos 1/JAVA/Tareas/I-Tarea-de-Algotitmos-Estructuras-de-Datos-I-Chat-/src/chat/usericon.png");
+        JLabel etiqueta_u=new JLabel();
+        etiqueta_u.setBounds(10,10,20,20);
+        etiqueta_u.setIcon(new ImageIcon(usericon.getImage().getScaledInstance(20,20, Image.SCALE_SMOOTH)));
+        add(etiqueta_u);
+        JLabel etiqueta_n=new JLabel("Nombre: ");
+        add(etiqueta_n);
+        nombre=new JTextField(15);
+        add(nombre);
+        JLabel etiqueta_c=new JLabel("Contacto: ");
+        add(etiqueta_c);
+        contacto=new JTextField(15);
+        add(contacto);
+        areaMensajes_c=new JTextArea(25,40);
+        add(areaMensajes_c);
+        areaTexto= new JTextField(30);
         add(areaTexto);
         enviarbtn =new JButton("ENVIAR");
         enviarMensaje evento_enviar=new enviarMensaje();
@@ -33,7 +51,7 @@ class canvasCliente extends JPanel{
         add(enviarbtn);
         }
         public class enviarMensaje implements ActionListener{
-        int puerto=9999;
+        int puerto=10000;
         public int get_puerto(){
             return puerto;
         }
@@ -55,12 +73,10 @@ class canvasCliente extends JPanel{
                 }
                 catch (IOException e1){
                     System.out.println("El puerto : "+ puerto +" se encuentra ocupado " + e1.getMessage());
-                    puerto-=1;//Modifica el puerto hasta encontrar uno que se encuentre
+                    puerto-=1;//Modifica el puerto hasta encontrar uno que se encuentre disponilble
                 }
             }
         }
     }
-    private JTextField areaTexto;
-
-    private JButton enviarbtn;
 }
+
--- src/chat/Servidor.java
+++ src/chat/Servidor.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import javax.swing.*;
 import java.awt.*;
 import java.io.DataInputStream;
-import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,30 +16,28 @@ public class Servidor {
 class ventanaServidor extends JFrame implements Runnable{
     public ventanaServidor(){
         setTitle("SERVIDOR");
-        setBounds(1200,300,280,350);
+        setBounds(1200,300,520,450);
         JPanel canvasServidor= new JPanel();
         canvasServidor.setLayout(new BorderLayout());
-        areamensajes=new JTextArea();
-        canvasServidor.add(areamensajes,BorderLayout.CENTER);
+        areamensajes_s=new JTextArea(25,40);
+        canvasServidor.add(areamensajes_s,BorderLayout.CENTER);
         add(canvasServidor);
         setVisible(true);
         Thread hilo_s=new Thread(this);//Hillo servidor
         hilo_s.start();
     }
-    private	JTextArea areamensajes;
+    private	JTextArea areamensajes_s;
 
     @Override
     public void run() {
         //System.out.println("FUNCIONANDO");
         while (true){
             try {
-                //chat.canvasCliente.enviarMensaje puertos=new chat.canvasCliente.enviarMensaje();
-                //ServerSocket servidor=new ServerSocket(puertos.get_puerto());
                 ServerSocket servidor = new ServerSocket( 9999);//Poner a escuchar
                 Socket socket_s=servidor.accept();//Aceptar las conexiones
                 DataInputStream flujo_e=new DataInputStream(socket_s.getInputStream());//Flujo de datos de entrada
                 String mensaje=flujo_e.readUTF();
-                areamensajes.append("\n"+mensaje);
+                areamensajes_s.append("\n"+mensaje);
                 socket_s.close();
             }
             catch (IOException e){
